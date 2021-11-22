766a87bd30b4a97c2391104246ac0164b292f459
d1brok <d1brok@de.ibm.com>
2021-11-22 11:47:26 +0100

    commit

--- s2i-buildpacks/server.js
+++ s2i-buildpacks/server.js
@@ -7,7 +7,7 @@ app.get('/', (request, response) => {
   response.send(`<!DOCTYPE html>
 <html>
   <head>
-    <title>Powered By Code Engine</title>
+    <title>Powered By Code Engine update</title>
   </head>
   <body>
     I was built by Buildpacks with Code Engine!
